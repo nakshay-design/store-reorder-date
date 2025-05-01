1e8c3932483dc3d62c90d17e83673db333d6e216
Kairav <kairav@storetransform.com>
2025-05-01 16:08:43 +0530

    get order data 1

--- api/getOrderData.js
+++ api/getOrderData.js
@@ -1,6 +1,10 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS'); // Allow GET and OPTIONS methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow Content-Type header
+
   const { orderId } = req.query; // Get order ID from query parameters
   const apiUrl = `https://ekta124.myshopify.com/admin/api/2023-10/orders/${orderId}.json`;
   const accessToken = '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';
